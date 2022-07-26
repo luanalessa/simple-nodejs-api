a28c84c0a7b8060ba657b9a868a0ebe33512cd25
Luana Lessa <lessalsn@gmail.com>
2022-07-25 21:18:16 -0300

    sahuashi

--- src/routers/index.js
+++ src/routers/index.js
@@ -3,7 +3,6 @@ import validateRequest from '../middlewares/validateRequest.js';
 import { createUserSchema } from '../schemas/user.schema.js';
 
 export default function router(app){
-
     app.get('/healthcheck', (req, res) => { res.send("Success!"); });
 
     app.post('/user', createUserHandler);
